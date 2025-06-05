e44641dd0fe50585afb2813c6b7a50f6e2822b4b
Dennis Reimer <reimer_dennis@web.de>
2025-06-05 19:04:48 +0200

    Trigger I build for OCR pipeline

--- ocr_pipeline.py
+++ ocr_pipeline.py
@@ -4,6 +4,10 @@ from PIL import Image
 import mlflow
 import json
 import os
+from sys import platform
+
+
+
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
@@ -12,6 +16,10 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 PDF_PATH = "pdf.pdf"
 OUTPUT_JSON = "ocr_output.json"
 
+if platform.startswith("win"):
+    poppler_path = r"C:\Tools\poppler-23\poppler-24.08.0\Library\bin"
+else:
+    poppler_path = None  # unter Linux: kein Pfad nötig
 # ==== PDF in Bilder umwandeln ====
 print(f"📄 Lade PDF: {PDF_PATH}")
 pages = convert_from_path(PDF_PATH, dpi=300, poppler_path=r"C:\Tools\poppler-23\poppler-24.08.0\Library\bin")
@@ -24,7 +32,7 @@ ocr_data = {
 
 for i, page in enumerate(pages):
     print(f"Verarbeite Seite {i+1}/{len(pages)}...")
-    text = pytesseract.image_to_string(page)
+    text = pytesseract.image_to_string(page, lang="deu")
     lines = [line.strip() for line in text.split("\n") if line.strip()]
     
     ocr_data["pages"].append({
@@ -36,7 +44,7 @@ for i, page in enumerate(pages):
 with open(OUTPUT_JSON, "w", encoding="utf-8") as f:
     json.dump(ocr_data, f, indent=2, ensure_ascii=False)
 
-print(f"✅ OCR-Ergebnis gespeichert unter: {OUTPUT_JSON}")
+print(f"OCR-Ergebnis gespeichert unter: {OUTPUT_JSON}")
 
 # ==== MLflow Logging ====
 mlflow.start_run(run_name="pdf_ocr_run")
